753b6828e81a3aadded4bd2c06b58ed6877f51f9
MaxewNazik <megamaxim2002@gmail.com>
2024-06-11 03:50:41 +0500

    again

--- nika/settings.py
+++ nika/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import dj_database_url
-import os
 
 from pathlib import Path
 
@@ -25,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-go3=wyz4k96w^in(#5)kot*)bix1&f@_ke)jv*n#nn++lylvj)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -51,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,8 +83,6 @@ WSGI_APPLICATION = 'nika.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -98,13 +93,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
-DATABASES={
-    'default':dj_database_url.parse("postgres://nika_689z_user:ETFl63hwQRgEpl8kOJatRFNUMw2HQ6bf@dpg-cpjma8v109ks73eopa90-a.frankfurt-postgres.render.com/nika_689z")
-}
 
-# postgres://nika_689z_user:ETFl63hwQRgEpl8kOJatRFNUMw2HQ6bf@dpg-cpjma8v109ks73eopa90-a.frankfurt-postgres.render.com/nika_689z
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -140,11 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /"staticfiles"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS=[BASE_DIR / 'static']
 
 
