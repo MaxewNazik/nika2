470bb77099710b38cf5e80f488f195db8e82b5fb
MaxewNazik <megamaxim2002@gmail.com>
2024-06-11 05:22:35 +0500

    2332

--- nika/settings.py
+++ nika/settings.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
-
 import dj_database_url
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-go3=wyz4k96w^in(#5)kot*)bix1&f@_ke)jv*n#nn++lylvj)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
